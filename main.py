a13df8b5b6e0aa80451e0e57e06e49567c37a0d9
Jonathan Richter <jsr2021@stanford.edu>
2022-10-23 21:23:06 -0700

    Added a line to main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     test = 5
+    test1 = 5
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
