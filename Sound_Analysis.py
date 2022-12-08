2472a1484a3ab7d60ef6eafc1b726641f552ec62
Jonathan Richter <jsr2021@stanford.edu>
2022-12-07 22:25:15 -0800

    updated the pipeline class and sound analysis class

--- Sound_Analysis.py
+++ Sound_Analysis.py
@@ -14,6 +14,7 @@ class Sound_Analysis:
         self.attributes = {'num_channels': 0, 'sample_width': 0, 'frame_rate': 0, 'frame_width': 0, 'length': 0,
                            'frame_count': 0, 'intensity': 0}
         self.librosa_rms_norm = None
+        self.librosa_rms_time = None
 
 
     def create_obj(self, path_and_song, song_name):
@@ -26,14 +27,21 @@ class Sound_Analysis:
         #if self.attributes['num_channels'] == 2:
         #    bool_mono = False #two channel = 16 bytes single channel = 8 bytes
         # Librosa Audio                                          #Lowering Frame Rate (Default is 44,000 per sec sr=self.attributes['frame_count']
-        librosa_audio_ts, sample_rate = librosa.load(path_and_song, sr=1000, mono=bool_mono)
+        song_sample_rate = 1000
+        librosa_audio_ts, sample_rate = librosa.load(path_and_song, sr=song_sample_rate, mono=bool_mono)
         fr_len = len(librosa_audio_ts)
-        librosa_rms = librosa.feature.rms(y=librosa_audio_ts, frame_length=fr_len).T
-        librosa_rms_norm = librosa.util.normalize(S=librosa_rms)
-        t = np.linspace(0, (self.attributes['length'] / 1000), len(librosa_rms_norm))
-        self.librosa_rms_norm = librosa_rms_norm
-        self.librosa_rms_time = t
-        self.plot_rms(librosa_rms_norm, t)
+        # librosa_rms = librosa.feature.rms(y=librosa_audio_ts, frame_length=fr_len).T
+        # librosa_rms_norm = librosa.util.normalize(S=librosa_rms)
+        # t = np.linspace(0, (self.attributes['length'] / song_sample_rate), len(librosa_rms_norm))
+        self.librosa_rms_norm = 1 #librosa_rms_norm
+        self.librosa_rms_time = 1 #t
+
+        chroma_cq = librosa.feature.chroma_cqt(y=librosa_audio_ts, sr=sample_rate, n_octaves=1)
+        chroma_diff = np.diff(chroma_cq, n=1, axis=1)
+        chroma_max_dat = np.abs(chroma_diff).max(axis=1)
+        self.chroma_max_dat = chroma_max_dat
+        self.chroma_cq = chroma_cq
+        #self.plot_rms(librosa_rms_norm, t)
 
 
     def attach_attributes(self, sound):
@@ -51,6 +59,8 @@ class Sound_Analysis:
         plt.title(self.song_name)
         plt.xlabel("Song Time [S]")
         plt.ylabel("Normalized RMS of Volume/Intensity")
+        #plt.savefig(self.song_name + ".png")
         plt.show()
+
 
 
